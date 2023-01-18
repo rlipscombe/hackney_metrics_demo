598bd33e2a80ac142e834433566aa2ef866f638b
Roger Lipscombe <roger@differentpla.net>
2023-01-18 21:29:31 +0000

    prometheus exporter

--- src/hackney_metrics_demo_app.erl
+++ src/hackney_metrics_demo_app.erl
@@ -9,7 +9,17 @@
 
 -export([start/2, stop/1]).
 
+-include_lib("kernel/include/logger.hrl").
+
+-define(PORT, 8126).
+
 start(_StartType, _StartArgs) ->
+    Dispatch =
+        cowboy_router:compile([{'_',
+                                [{"/metrics/[:registry]", prometheus_cowboy2_handler, []}]}]),
+    {ok, _} = cowboy:start_clear(http, [{port, ?PORT}], #{env => #{dispatch => Dispatch}}),
+    ?LOG_INFO("Cowboy server listening on port ~p", [?PORT]),
+
     hackney_metrics_demo_sup:start_link().
 
 stop(_State) ->
