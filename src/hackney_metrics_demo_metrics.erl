c5bd0ef66cb004d222a181c15da5320a0c5f5610
Roger Lipscombe <roger@differentpla.net>
2023-01-18 21:08:21 +0000

    Write metrics to logger

--- src/hackney_metrics_demo_metrics.erl
+++ src/hackney_metrics_demo_metrics.erl
@@ -3,18 +3,24 @@
 -export([new/2, delete/1, increment_counter/1, increment_counter/2, decrement_counter/1,
          decrement_counter/2, update_histogram/2, update_gauge/2, update_meter/2]).
 
+-include_lib("kernel/include/logger.hrl").
+
 new(_Type, _Name) -> ok.
 delete(_Name) -> ok.
 
 increment_counter(Name) -> increment_counter(Name, 1).
-increment_counter(_Name, _Value) -> ok.
+increment_counter(Name, Value) -> ?LOG_INFO(#{f => ?FUNCTION_NAME, name => Name, value => Value}).
 decrement_counter(Name) -> decrement_counter(Name, 1).
-decrement_counter(_Name, _Value) -> ok.
+decrement_counter(Name, Value) -> ?LOG_INFO(#{f => ?FUNCTION_NAME, name => Name, value => Value}).
 
-update_histogram(_Name, Fun) when is_function(Fun, 0) ->
-    Fun();
-update_histogram(_Name, Value) when is_number(Value) ->
-    ok.
+update_histogram(Name, Fun) when is_function(Fun, 0) ->
+    Begin = os:timestamp(),
+    Result = Fun(),
+    Duration = timer:now_diff(os:timestamp(), Begin) div 1000,
+    ?LOG_INFO(#{f => ?FUNCTION_NAME, name => Name, value => Duration}),
+    Result;
+update_histogram(Name, Value) when is_number(Value) ->
+    ?LOG_INFO(#{f => ?FUNCTION_NAME, name => Name, value => Value}).
 
-update_gauge(_Name, _Value) -> ok.
-update_meter(_Name, _Value) -> ok.
+update_gauge(Name, Value) -> ?LOG_INFO(#{f => ?FUNCTION_NAME, name => Name, value => Value}).
+update_meter(Name, Value) -> ?LOG_INFO(#{f => ?FUNCTION_NAME, name => Name, value => Value}).
